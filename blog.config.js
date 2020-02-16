ca2e30a4b70e5b1aada2791b1a83796f48d44246
dylanholmes <dylanh2@uw.edu>
2020-02-16 12:29:02 -0800

    Initial commit.

---
+++ blog.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMeta: {
+    title: 'dylanholm.es',
+    author: 'Dylan Holmes',
+    image: '/static/site-feature.png',
+    description: 'A blog.',
+    siteUrl: 'http://dylanholm.es',
+    social: {
+      twitter: '_jolvera',
+    },
+    postsPerPage: 5,
+  },
+}
